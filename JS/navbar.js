7ee46e08aeee8fdca24a0afa9d681a659773cc20
Evander Tokang <102147928+vanderoo@users.noreply.github.com>
2023-04-12 02:32:14 +0700

    Add files via upload

---
+++ JS/navbar.js
@@ -0,0 +1,66 @@
+const arrowProfile = document.querySelector("#arrow-profile");
+const profile = document.querySelector(".profile-container");
+const hamburger = document.querySelector(".hamburger")
+const sidebar = document.querySelector(".navbar-hamburger-container");
+const body = document.querySelector('body');
+const back = document.querySelector(".back");
+const sideHome = document.querySelector("#side-home");
+const sideCourse = document.querySelector("#side-course");
+const sideMyCourse = document.querySelector("#side-mycourse");
+const menuProfile = document.querySelector(".profile-menu");
+const currentUrl = window.location.href;
+
+
+// untuk background pada menu sidebar yang halamannya sedang terbuka
+if (currentUrl.includes("index.html")){
+    sideHome.style.background = "rgb(73, 187, 189, 0.5)";
+    sideHome.style.color = "white";
+}else if(currentUrl.includes("MyCourse.html")){
+    sideMyCourse.style.background = "rgb(73, 187, 189, 0.5)";
+    sideMyCourse.style.color = "white";
+}else if(currentUrl.includes("Course.html")){
+    sideCourse.style.background = "rgb(73, 187, 189, 0.5)";
+    sideCourse.style.color = "white";
+}
+
+// menambah class active pada arrowProfile untuk transform dan show untuk dropdown
+profile.addEventListener("click", () => {
+    console.log("tes");
+    arrowProfile.classList.toggle("active");
+    menuProfile.classList.toggle("show");
+});
+
+
+// Untuk memunculkan dan menghilangkan sidebar
+hamburger.addEventListener("click", () => {
+    console.log("tes");
+    sidebar.classList.add("active");
+    body.style.overflow = 'hidden';
+});
+
+back.addEventListener("click", () => {
+    console.log("tes");
+    sidebar.classList.remove("active");
+    body.style.overflow = 'auto';
+});
+
+
+
+
+// Baca media query
+let mediaQuery = window.matchMedia("(max-width: 900px)");
+
+function handleMediaChange(e) {
+    if (e.matches) {
+        console.log("matches");
+        menuProfile.classList.remove("show");
+        arrowProfile.classList.remove("active");
+    } else {
+        console.log("not matches");
+        sidebar.classList.remove("active");
+    }
+}
+
+mediaQuery.addEventListener('change', handleMediaChange);
+
+
