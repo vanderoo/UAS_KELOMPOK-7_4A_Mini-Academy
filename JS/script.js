3127f36de055daf25602c5085aa0d4a661c519c3
Evander Tokang <vandertokang17@gmail.com>
2023-05-19 17:17:29 +0700

    Login page

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,4 @@
-window.onload = function(){
+/* window.onload = function(){
 
     const btn_log = document.querySelector('.login-container');
     const btn_reg = document.querySelector('.register-container');
@@ -18,5 +18,37 @@ window.onload = function(){
     }
     
 }
+*/
+
+window.onload = function(){
+
+const logBtn = document.querySelector("#log-btn");
+const regBtn = document.querySelector("#reg-btn");
+const regForm = document.querySelector(".register-form");
+const logForm = document.querySelector(".remember-forgot");
+var btn = document.querySelector("#btn-box");
+
+
+regBtn.addEventListener("click", () => {
+    regBtn.classList.add("active");
+    logBtn.classList.remove("active");
+    btn.style.left = '10vw';
+    regForm.style.display = 'inline';
+    logForm.style.display = 'none';
+});
+
+logBtn.addEventListener("click", () => {
+    regBtn.classList.remove("active");
+    logBtn.classList.add("active");
+    btn.style.left = '0';
+    regForm.style.display = 'none';
+    logForm.style.display = 'flex';
+});
+}
+
+
+
+
+
 
 
