d57d3a7860140ff1b33d39d1bf37158401470ed9
Evander Tokang <102147928+vanderoo@users.noreply.github.com>
2023-05-20 12:06:55 +0700

    Update navbar.js

--- JS/navbar.js
+++ JS/navbar.js
@@ -9,59 +9,71 @@ const sideCourse = document.querySelector("#side-course");
 const sideMyCourse = document.querySelector("#side-mycourse");
 const menuProfile = document.querySelector(".profile-menu");
 const currentUrl = window.location.href;
+const nav = document.querySelector(".navbar-container");
+const navHeight = document.querySelector(".navbar-height");
 
+var navbarHeight = nav.offsetHeight;
 
-// untuk background pada menu sidebar yang halamannya sedang terbuka
-if (currentUrl.includes("index.html")){
-    sideHome.style.background = "rgb(73, 187, 189, 0.5)";
-    sideHome.style.color = "white";
-}else if(currentUrl.includes("MyCourse.html")){
-    sideMyCourse.style.background = "rgb(73, 187, 189, 0.5)";
-    sideMyCourse.style.color = "white";
-}else if(currentUrl.includes("Course.html")){
-    sideCourse.style.background = "rgb(73, 187, 189, 0.5)";
-    sideCourse.style.color = "white";
+function getNavHeight(){
+    navbarHeight = nav.offsetHeight;
+    navHeight.style.height = `${navbarHeight}px`;
 }
 
-// menambah class active pada arrowProfile untuk transform dan show untuk dropdown
-profile.addEventListener("click", () => {
-    console.log("tes");
-    arrowProfile.classList.toggle("active");
-    menuProfile.classList.toggle("show");
-});
+navHeight.style.height = `${navbarHeight}px`;
 
-
-// Untuk memunculkan dan menghilangkan sidebar
-hamburger.addEventListener("click", () => {
-    console.log("tes");
-    sidebar.classList.add("active");
-    body.style.overflow = 'hidden';
-});
-
-back.addEventListener("click", () => {
-    console.log("tes");
-    sidebar.classList.remove("active");
-    body.style.overflow = 'auto';
-});
-
-
-
-
-// Baca media query
-let mediaQuery = window.matchMedia("(max-width: 900px)");
-
-function handleMediaChange(e) {
-    if (e.matches) {
-        console.log("matches");
-        menuProfile.classList.remove("show");
-        arrowProfile.classList.remove("active");
-    } else {
-        console.log("not matches");
-        sidebar.classList.remove("active");
-        body.style.overflow = 'auto';
+window.onload = () => {
+    window.addEventListener("resize",getNavHeight);
+    
+    // untuk background pada menu sidebar yang halamannya sedang terbuka
+    if (currentUrl.includes("index.html")){
+        sideHome.style.background = "rgb(73, 187, 189, 0.5)";
+        sideHome.style.color = "white";
+    }else if(currentUrl.includes("MyCourse.html")){
+        sideMyCourse.style.background = "rgb(73, 187, 189, 0.5)";
+        sideMyCourse.style.color = "white";
+    }else if(currentUrl.includes("Course.html")){
+        sideCourse.style.background = "rgb(73, 187, 189, 0.5)";
+        sideCourse.style.color = "white";
     }
-}
+    
+    // menambah class active pada arrowProfile untuk transform dan show untuk dropdown
+    profile.addEventListener("click", () => {
+        console.log("tes");
+        arrowProfile.classList.toggle("active");
+        menuProfile.classList.toggle("show");
+    });
 
-mediaQuery.addEventListener('change', handleMediaChange);
+    // Untuk memunculkan dan menghilangkan sidebar
+    hamburger.addEventListener("click", () => {
+        sidebar.classList.add("active");
+        body.style.overflow = 'hidden';
+        console.log(window.scrollY);
+    });
+    
+    back.addEventListener("click", () => {
+        sidebar.classList.remove("active");
+        body.style.overflowY = 'auto';
+    });
+    
+    
+    
+    
+    // Baca media query
+    let mediaQuery = window.matchMedia("(max-width: 900px)");
+    function handleMediaChange(e) {
+        if (e.matches) {
+            console.log("matches");
+            menuProfile.classList.remove("show");
+            arrowProfile.classList.remove("active");
+        } else {
+            console.log("not matches");
+            sidebar.classList.remove("active");
+            
+        }
+    }
+    mediaQuery.addEventListener('change', handleMediaChange);
+    //
+
+}
 
 
